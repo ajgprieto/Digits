44cc2e90fc258173be5a29b277df0ae9d395b39a
Alvin Prieto <aj.garidanprieto@gmail.com>
2013-11-12 18:44:34 -1000

    finish WOD

--- app/Global.java
+++ app/Global.java
@@ -5,31 +5,40 @@ import play.*;
 import views.formdata.ContactFormData;
 
 /**
- * Global class.  Provides Contacts that are included on startup.
+ * Global class. Provides Contacts that are included on startup.
+ * 
  * @author AJ
- *
+ * 
  */
 public class Global extends GlobalSettings {
-  
-  
+
   /**
    * Adds the four contacts to the database on startup.
+   * 
+   * @param app the application.
    */
   public void onStart(Application app) {
-  
-    Contact contact1 = new Contact(1L, "Philip", "Johnson", "123-456-7890", "Home");
-    Contact contact2 = new Contact(2L, "Jane", "Doe", "477-456-7890", "Work");
-    Contact contact3 = new Contact(3L, "Justin", "Verlander", "999-456-8888", "Home");
-    Contact contact4 = new Contact(4L, "Gordie", "Howe", "654-456-2345", "Mobile");
+
+    String email = Play.application().configuration().getString("admin.email");
+    String password = Play.application().configuration().getString("admin.password");
     
-    UserInfoDB.addUserInfo("John Smith", "smith@example.com", "password");
-    UserInfoDB.addUserInfo("John Jacob", "jacob@example.com", "password");
+    System.out.println(email + " " + password);
+    UserInfoDB.defineAdmin("Admin", email, password);
     
-    ContactDB.add("smith@example.com", new ContactFormData(contact1));
-    ContactDB.add("smith@example.com", new ContactFormData(contact2));
-    ContactDB.add("jacob@example.com", new ContactFormData(contact3));
-    ContactDB.add("jacob@example.com", new ContactFormData(contact4));
-       
+    if (UserInfoDB.hasAdmin()) {
+      Contact contact1 = new Contact(1L, "Philip", "Johnson", "123-456-7890", "Home");
+      Contact contact2 = new Contact(2L, "Jane", "Doe", "477-456-7890", "Work");
+      Contact contact3 = new Contact(1L, "Justin", "Verlander", "999-456-8888", "Home");
+      Contact contact4 = new Contact(2L, "Gordie", "Howe", "654-456-2345", "Mobile");
+
+      UserInfoDB.addUserInfo("John Smith", "smith@example.com", "password");
+      UserInfoDB.addUserInfo("John Jacob", "jacob@example.com", "password");
+
+      ContactDB.add("smith@example.com", new ContactFormData(contact1));
+      ContactDB.add("smith@example.com", new ContactFormData(contact2));
+      ContactDB.add("jacob@example.com", new ContactFormData(contact3));
+      ContactDB.add("jacob@example.com", new ContactFormData(contact4));
+    }
   }
 
 }
--- app/models/UserInfoDB.java
+++ app/models/UserInfoDB.java
@@ -4,26 +4,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Provides an in-memory repository for UserInfo.
- * Storing credentials in the clear is kind of bogus.
+ * Provides an in-memory repository for UserInfo. Storing credentials in the clear is kind of bogus.
+ * 
  * @author Philip Johnson
  */
 public class UserInfoDB {
-  
+
   private static Map<String, UserInfo> userinfos = new HashMap<String, UserInfo>();
-  
+  private static boolean haveAdmin = false;
+
+  /**
+   * Defines an admin account.
+   * 
+   * @param name the admin name
+   * @param email the admin email
+   * @param password the admin password
+   */
+  public static void defineAdmin(String name, String email, String password) {
+    if (name != null && email != null && password != null) {
+      addUserInfo(name, email, password);
+      haveAdmin = true;
+    }
+  }
+
+  /**
+   * Checks if app has an admin.
+   * 
+   * @return true if admin credentials defined.
+   */
+  public static boolean hasAdmin() {
+    return haveAdmin;
+  }
+
   /**
    * Adds the specified user to the UserInfoDB.
+   * 
    * @param name Their name.
    * @param email Their email.
-   * @param password Their password. 
+   * @param password Their password.
    */
   public static void addUserInfo(String name, String email, String password) {
     userinfos.put(email, new UserInfo(name, email, password));
   }
-  
+
   /**
    * Returns true if the email represents a known user.
+   * 
    * @param email The email.
    * @return True if known user.
    */
@@ -33,6 +59,7 @@ public class UserInfoDB {
 
   /**
    * Returns the UserInfo associated with the email, or null if not found.
+   * 
    * @param email The email.
    * @return The UserInfo.
    */
@@ -42,17 +69,12 @@ public class UserInfoDB {
 
   /**
    * Returns true if email and password are valid credentials.
-   * @param email The email. 
-   * @param password The password. 
+   * 
+   * @param email The email.
+   * @param password The password.
    * @return True if email is a valid user email and password is valid for that email.
    */
   public static boolean isValid(String email, String password) {
-    return ((email != null) 
-            &&
-            (password != null) 
-            &&
-            isUser(email) 
-            &&
-            getUser(email).getPassword().equals(password));
+    return ((email != null) && (password != null) && isUser(email) && getUser(email).getPassword().equals(password));
   }
 }
